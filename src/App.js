6a3206fdea1e7a07928eab449c167d15874ef0ab
victorcode <vdcloayza@gmail.com>
2022-06-03 23:54:22 -0400

    practicando formularios con react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Saludos from './Components/Saludos';
+import { useEffect, useState } from 'react';
+import Todo from './Todo';
+
+
 
 function App() {
+  
+  
+  const Users={
+   
+    age:27,
+    color:"red"
+  }
+  const greetings=(name)=>{
+
+    console.log("Saludando a todos"+name);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Todo/>
+        <p>Passing props</p>
+        <Saludos name={Users.Name} age="30" 
+        UseState={useState}
+        users={Users} greetings={greetings} 
+        />
       </header>
     </div>
   );
